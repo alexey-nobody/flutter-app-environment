d9d533de684470054e92ebff2e629855c680a31a
Alexey Lepskii <alexey.lepskii@gmail.com>
2023-02-26 18:26:59 +0300

    example for handle environment variables with json

---
+++ example/README.md
@@ -0,0 +1,7 @@
+# Example project used Flutter App Environment
+
+## Example with json file for handle environment variables
+
+```sh
+flutter run -t lib/main_development.dart
+```
---
+++ example/lib/pages/home_page.dart
@@ -0,0 +1,45 @@
+import 'package:example/models/environment_from_json_config.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_app_environment/environment.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({required this.title, super.key});
+
+  final String title;
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int _counter =
+      Environment<EnvironmentFromJsonConfig>.instance().config.initialCounter;
+
+  void _incrementCounter() {
+    setState(() => _counter++);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text(widget.title)),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            const Text('You have pushed the button this many times:'),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headlineMedium,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _incrementCounter,
+        tooltip: 'Increment',
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
